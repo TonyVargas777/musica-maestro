06cbdd75df88da0b82251e90770c5177ed9e95e1
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2024-01-14 01:31:39 +0100

    compositores

--- src/Compositores.jsx
+++ src/Compositores.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
 
 function Compositores({ compositores }) {
   return (
     <>
       {compositores.map((compositor, index) => (
         <>
-        <h2 key={index}>{compositor.nombre}</h2>
-        <h2 key={index}><li>{compositor.fecha}</li></h2>       
+          <ul>
+            <h2 key={index}>{compositor.nombre}</h2>
+            <h2 key={index}>
+              <li>{compositor.fecha}</li>
+            </h2>
             {compositor.canciones.map((cancion, idx) => (
-              <ul><li key={idx}>{cancion}</li></ul>
-            ))}          
+              <ul>
+                <li key={idx}>{cancion}</li>
+              </ul>
+            ))}
+          </ul>
         </>
       ))}
     </>
